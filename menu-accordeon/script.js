c6dff75e4771f8dc6ea6451b7463ebb6384162f4
Ibrahim_DeV <me.devs15@gmail.com>
2024-02-21 16:11:47 +0100

    ajout js qui permet toggle affichage

--- menu-accordeon/script.js
+++ menu-accordeon/script.js
@@ -1,1 +1,38 @@
 console.log('Hello World')
+
+const elmNav = [...document.querySelectorAll('.list')]
+const quest = document.querySelectorAll('.question')
+
+const nav = elmNav.slice(1)
+
+activElm(nav, 'active-list')
+toggle(quest)
+
+function toggle(qus){
+    qus.forEach((item) => {
+        item.addEventListener('click', () => {
+            item.nextElementSibling.classList.toggle('voir')
+            
+            if (item.nextElementSibling.classList.contains('voir')) {
+        
+                item.lastElementChild.src = "./assets/icon/F7ChevronUp.svg"
+                console.log('oui')
+            }else {
+                item.lastElementChild.src = "./assets/icon/F7ChevronDown.svg"
+            }
+            
+        })
+    })
+}
+
+
+function activElm (list, name) {
+    list.forEach((item) => {
+
+        item.addEventListener('click', () => {
+    
+            nav.forEach((item) => item.classList.remove(name))
+            item.classList.add(name)
+        })
+    })
+}
