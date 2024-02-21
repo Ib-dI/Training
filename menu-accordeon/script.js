0e70430af20b6b8c81d6a215befab923e433bbcc
Ibrahim_DeV <me.devs15@gmail.com>
2024-02-21 22:12:56 +0100

    Fin code js/css menu accordeon

--- menu-accordeon/script.js
+++ menu-accordeon/script.js
@@ -2,12 +2,59 @@ console.log('Hello World')
 
 const elmNav = [...document.querySelectorAll('.list')]
 const quest = document.querySelectorAll('.question')
-
 const nav = elmNav.slice(1)
+const faContent = [...document.querySelectorAll('.content')]
 
-activElm(nav, 'active-list')
 toggle(quest)
 
+nav.forEach(item => {
+    item.addEventListener('click',(e)=>{
+        const target = e.target;
+        
+        const move = nav.findIndex(item => item.classList.contains('active-list'))
+        const show = nav.indexOf(target)
+
+        faContent[move].classList.remove('active-content')
+        nav[move].classList.remove('active-list')
+        faContent[show].classList.add('active-content')
+        nav[show].classList.add('active-list')
+    })
+
+    const children = [...item.children];
+    if (children) {
+        children.forEach(child => {
+            child.addEventListener('click', (e)=>{
+                const target = e.target;
+                e.stopPropagation();
+
+                const indexRemove = nav.findIndex(item => item.classList.contains('active-list'))
+                const indexToShow = nav.indexOf(target.parentNode)
+                
+                faContent[indexRemove].classList.remove('active-content')
+
+                nav[indexRemove].classList.remove('active-list')
+    
+                faContent[indexToShow].classList.add('active-content')
+                nav[indexToShow].classList.add('active-list')
+            });
+        });
+    }
+    
+});
+
+// function transit (e){
+//     const indexRemove = nav.findIndex(item => item.classList.contains('active-list'))
+
+//     const indexToShow = nav.indexOf(e.target)
+
+//     faContent[indexRemove].classList.remove('active-content')
+
+//     nav[indexRemove].classList.remove('active-list')
+
+//     faContent[indexToShow].classList.add('active-content')
+//     nav[indexToShow].classList.add('active-list')
+
+// }
 function toggle(qus){
     qus.forEach((item) => {
         item.addEventListener('click', () => {
@@ -16,7 +63,6 @@ function toggle(qus){
             if (item.nextElementSibling.classList.contains('voir')) {
         
                 item.lastElementChild.src = "./assets/icon/F7ChevronUp.svg"
-                console.log('oui')
             }else {
                 item.lastElementChild.src = "./assets/icon/F7ChevronDown.svg"
             }
@@ -24,7 +70,6 @@ function toggle(qus){
         })
     })
 }
-
 
 function activElm (list, name) {
     list.forEach((item) => {
