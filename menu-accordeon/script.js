ce66e503eb0a0da387a243dd40ab1e73264efa75
Ibrahim_DeV <me.devs15@gmail.com>
2024-02-22 08:23:35 +0100

    ajout de deux fonction

--- menu-accordeon/script.js
+++ menu-accordeon/script.js
@@ -8,53 +8,44 @@ const faContent = [...document.querySelectorAll('.content')]
 toggle(quest)
 
 nav.forEach(item => {
-    item.addEventListener('click',(e)=>{
-        const target = e.target;
-        
-        const move = nav.findIndex(item => item.classList.contains('active-list'))
-        const show = nav.indexOf(target)
-
-        faContent[move].classList.remove('active-content')
-        nav[move].classList.remove('active-list')
-        faContent[show].classList.add('active-content')
-        nav[show].classList.add('active-list')
-    })
+    item.addEventListener('click',transitParent)
 
     const children = [...item.children];
     if (children) {
         children.forEach(child => {
-            child.addEventListener('click', (e)=>{
-                const target = e.target;
-                e.stopPropagation();
-
-                const indexRemove = nav.findIndex(item => item.classList.contains('active-list'))
-                const indexToShow = nav.indexOf(target.parentNode)
-                
-                faContent[indexRemove].classList.remove('active-content')
-
-                nav[indexRemove].classList.remove('active-list')
-    
-                faContent[indexToShow].classList.add('active-content')
-                nav[indexToShow].classList.add('active-list')
-            });
+            child.addEventListener('click', transitChild);
         });
     }
     
 });
 
-// function transit (e){
-//     const indexRemove = nav.findIndex(item => item.classList.contains('active-list'))
+function transitParent (e){
+    const indexRemove = nav.findIndex(item => item.classList.contains('active-list'))
 
-//     const indexToShow = nav.indexOf(e.target)
+    const indexToShow = nav.indexOf(e.target)
 
-//     faContent[indexRemove].classList.remove('active-content')
+    faContent[indexRemove].classList.remove('active-content')
 
-//     nav[indexRemove].classList.remove('active-list')
+    nav[indexRemove].classList.remove('active-list')
 
-//     faContent[indexToShow].classList.add('active-content')
-//     nav[indexToShow].classList.add('active-list')
+    faContent[indexToShow].classList.add('active-content')
+    nav[indexToShow].classList.add('active-list')
+}
+function transitChild(e) {
+    const target = e.target;
+    e.stopPropagation();
 
-// }
+    const indexRemove = nav.findIndex(item => item.classList.contains('active-list'))
+    const indexToShow = nav.indexOf(target.parentNode)
+    
+    faContent[indexRemove].classList.remove('active-content')
+
+    nav[indexRemove].classList.remove('active-list')
+
+    faContent[indexToShow].classList.add('active-content')
+    nav[indexToShow].classList.add('active-list')
+
+}
 function toggle(qus){
     qus.forEach((item) => {
         item.addEventListener('click', () => {
